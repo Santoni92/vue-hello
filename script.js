066c3a1d2f7b7c75bd26f651173d9632916d1533
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-24 17:00:38 +0100

    modifica

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log('JS VUE OK!');
-
+/*
 const riferimento = new Vue(
     {
         el : '#saluto',
@@ -18,6 +18,21 @@ const altroRiferimento = new Vue({
     data:{
         pathImg: 'img/tigre.jpg'  /*aggiunta variabile nella struttura dati per indicare il path dell'immagine 
                                     ed utilizzata la direttiva v-bind per stampare l'attributo 'src' del tag 
-                                    img */
+                                    img *//*
     }
 });
+
+*/
+
+const riferimento = new Vue(
+    {
+        el : '#saluto',
+        data: {
+                messaggio: 'Hello World',
+                pathImg:'img/tigre.jpg'
+                  
+                  
+             }
+    }
+    
+);
