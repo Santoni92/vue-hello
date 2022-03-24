fa6d7793901eecba047b6ba5f772fcebec74d506
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-24 16:10:51 +0100

    aggiunto oggetto vue

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('JS VUE OK!');
+
+const riferimento = new Vue(
+    {
+        el : '#saluto',
+        data: {
+                messaggio: 'Hello World'
+             }
+    }
+    
+);
